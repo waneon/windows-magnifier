c5de0e55e00ae0810810458f7eb8742332078071
waneon <waneon.kim@gmail.com>
2021-07-05 17:16:14 +0900

    Support input transform feature

--- src/maginfo.cpp
+++ src/maginfo.cpp
@@ -1,45 +1,44 @@
-#include <windows.h>
 #include "maginfo.h"
+
+#include <windows.h>
 
 const int DEFAULT_MAGNIFICATION_FACTOR = 2;
 const int DEFAULT_DEAD_ZONE = 200;
 
 MagInfo::MagInfo() {
-	int x = GetSystemMetrics(SM_CXSCREEN);
-	int y = GetSystemMetrics(SM_CYSCREEN);
-	int k = DEFAULT_DEAD_ZONE;
-	f = DEFAULT_MAGNIFICATION_FACTOR;
+    int x = GetSystemMetrics(SM_CXSCREEN);
+    int y = GetSystemMetrics(SM_CYSCREEN);
+    int k = DEFAULT_DEAD_ZONE;
+    f = DEFAULT_MAGNIFICATION_FACTOR;
 
-	mul_x = x * (1 - 1 / f) / (x - 2 * k);
-	mul_y = y * (1 - 1 / f) / (y - 2 * k);
-	sub_x = mul_x * k;
-	sub_y = mul_y * k;
-	max_x = x * (1 - 1 / f);
-	max_y = y * (1 - 1 / f);
+    mul_x = x * (1 - 1 / f) / (x - 2 * k);
+    mul_y = y * (1 - 1 / f) / (y - 2 * k);
+    sub_x = mul_x * k;
+    sub_y = mul_y * k;
+    max_x = x * (1 - 1 / f);
+    max_y = y * (1 - 1 / f);
 }
 
 int MagInfo::TransformX(int x) const {
-	int tx = x * mul_x - sub_x;
+    int tx = x * mul_x - sub_x;
 
-	if (tx < 0)
-		tx = 0;
-	else if (tx > max_x)
-		tx = max_x;
-	
-	return tx;
+    if (tx < 0)
+        tx = 0;
+    else if (tx > max_x)
+        tx = max_x;
+
+    return tx;
 }
 
 int MagInfo::TransformY(int y) const {
-	int ty = y * mul_y - sub_y;
+    int ty = y * mul_y - sub_y;
 
-	if (ty < 0)
-		ty = 0;
-	else if (ty > max_y)
-		ty = max_y;
+    if (ty < 0)
+        ty = 0;
+    else if (ty > max_y)
+        ty = max_y;
 
-	return ty;
+    return ty;
 }
 
-float MagInfo::GetMagFactor() const {
-	return f;
-}
+float MagInfo::GetMagFactor() const { return f; }
--- src/main.cpp
+++ src/main.cpp
@@ -1,83 +1,96 @@
-#include <windows.h>
 #include <magnification.h>
-#include <iostream>
+#include <windows.h>
+
 #include <cstring>
-#include "version.h"
+#include <iostream>
+
 #include "maginfo.h"
+#include "version.h"
 
 const int TIMER_MS = 8;
 
 void Magnify(const MagInfo& i);
 void UnMagnify(const MagInfo& i);
 
-// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR pCmdLine, int nCmdShow) {
-int main(int argc, char *argv[]) {
-	// print version
-	if (argc == 2) {
-		if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v")) {
-			std::cout << VERSION << "\n";
+// int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR
+// pCmdLine, int nCmdShow) {
+int main(int argc, char* argv[]) {
+    // print version
+    if (argc == 2) {
+        if (!strcmp(argv[1], "--version") || !strcmp(argv[1], "-v")) {
+            std::cout << VERSION << "\n";
 
-			return 0;
-		}
-	}
+            return 0;
+        }
+    }
 
-	// get mag info
-	MagInfo i = {};
-	bool magnified = false;
+    // get mag info
+    MagInfo i = {};
+    bool magnified = false;
 
-	// turn off console
-	FreeConsole();
+    // turn off console
+    FreeConsole();
 
-	// init mag
-	if (!MagInitialize())
-		return 0;
-	// set toggle key
-	if (!RegisterHotKey(NULL, 0, MOD_ALT, '1'))
-		return 0;
-	if (!RegisterHotKey(NULL, 1, MOD_ALT, '2'))
-		return 0;
+    // init mag
+    if (!MagInitialize()) return 0;
+    // set toggle key
+    if (!RegisterHotKey(NULL, 0, MOD_ALT, '1')) return 0;
+    if (!RegisterHotKey(NULL, 1, MOD_ALT, '2')) return 0;
 
-	// check event loop
-	MSG msg = {};
-	while (GetMessage(&msg, NULL, 0, 0)) {
-		switch (msg.message) {
-			case WM_HOTKEY:
-				if (msg.wParam == 0) {
-					if (magnified) {
-						KillTimer(NULL, 1);
-						UnMagnify(i);
-					} else {
-						SetTimer(NULL, 1, TIMER_MS, NULL);
-					}
-					magnified = !magnified;
-				} else {
-					goto out;
-				}
-				break;
-			case WM_TIMER:
-				if (magnified)
-					Magnify(i);
-				break;
-		}
-	}
-	out:
+    // check event loop
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        switch (msg.message) {
+            case WM_HOTKEY:
+                if (msg.wParam == 0) {
+                    if (magnified) {
+                        KillTimer(NULL, 1);
+                        UnMagnify(i);
+                    } else {
+                        SetTimer(NULL, 1, TIMER_MS, NULL);
+                    }
+                    magnified = !magnified;
+                } else {
+                    goto out;
+                }
+                break;
+            case WM_TIMER:
+                if (magnified) Magnify(i);
+                break;
+        }
+    }
+out:
 
-	// un-set toggle key
-	UnregisterHotKey(NULL, 1);
-	UnregisterHotKey(NULL, 2);
-	// un-init mag
-	MagUninitialize();
+    // un-set toggle key
+    UnregisterHotKey(NULL, 1);
+    UnregisterHotKey(NULL, 2);
+    // un-init mag
+    MagUninitialize();
 
-	return 0;
+    return 0;
 }
 
 void Magnify(const MagInfo& i) {
-	POINT p = {};
-	GetCursorPos(&p);
+    POINT p = {};
+    GetCursorPos(&p);
 
-	MagSetFullscreenTransform(i.GetMagFactor(), i.TransformX(p.x), i.TransformY(p.y));
+	int x = i.TransformX(p.x), y = i.TransformY(p.y);
+
+    MagSetFullscreenTransform(i.GetMagFactor(), x, y);
+
+    RECT rcDest;
+    rcDest.left = 0;
+    rcDest.top = 0;
+    rcDest.right = GetSystemMetrics(SM_CXSCREEN);
+    rcDest.bottom = GetSystemMetrics(SM_CYSCREEN);
+
+    RECT rcSource;
+    rcSource.left = x;
+    rcSource.top = y;
+    rcSource.right = rcSource.left + (int)(rcDest.right / i.GetMagFactor());
+    rcSource.bottom = rcSource.top + (int)(rcDest.bottom / i.GetMagFactor());
+
+    MagSetInputTransform(TRUE, &rcSource, &rcDest);
 }
 
-void UnMagnify(const MagInfo& i) {
-	MagSetFullscreenTransform(1, 0, 0);
-}
+void UnMagnify(const MagInfo& i) { MagSetFullscreenTransform(1, 0, 0); }
